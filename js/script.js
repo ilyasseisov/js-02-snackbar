c1e7174add556754e5ab1163e32e00da448febbd
Ilyas Seisov <ilyasseisov@gmail.com>
2022-06-07 22:06:50 -0700

    final commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const showSnackbarBtn = document.querySelector('.show-snackbar-btn');
+const snackbar = document.querySelector('.snackbar');
+const closeBtn = document.querySelector('.snackbar-close-btn');
+
+showSnackbarBtn.addEventListener('click', () => {
+  snackbar.classList.add('isShown');
+
+  setTimeout(() => {
+    snackbar.classList.remove('isShown');
+  }, 5000);
+});
+
+closeBtn.addEventListener('click', () => {
+  snackbar.classList.remove('isShown');
+});
